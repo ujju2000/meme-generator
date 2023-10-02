ee63086e0329a6ca6ce34763f074f59a975c9717
Ujjwal Tarika <ujjutarika.35@gmail.com>
2023-10-02 12:10:14 +0530

    added meme generator project

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 
 export default function Footer() {
   return (
-    <footer className='w-full bg-black text-white p-4 text-2xl font-semibold'>
+    <footer className='w-full bg-black text-white p-4 text-2xl relative font-semibold'>
         &copy; Ujjwal Tarika
     </footer>
   )
